4c2b783f023f7a23488e47a07038c3d3bceee138
Amar Singh <ummerr@gmail.com>
2014-12-24 16:31:44 -0500

    replaced the faq with an about page

--- app.rb
+++ app.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
+#shotgun sends this to port 9393
 
 get '/' do
-  @title = "This is a test title"
+  @title = "Comparative You"
   erb :index
 end
+
+get '/about' do
+  erb :about
+end
+
+
+#testing routes/logic not for production
+get '/hello/:name/' do
+  "hello there #{params[:name].upcase} "
+end
+
+get '/hello/:name/:city' do
+  "hello there #{params[:name].upcase} from #{params[:city].upcase}"
+end
+
+# no idea how this works, but references the splat argument
+# so if more than one thing is passed from the url, it can hold all of them via the '*' wildcard
+get '/more/*' do
+  params[:splat]
+end
+#
---
+++ features-needed.md
@@ -0,0 +1,14 @@
+##notes
+
+-do not add layout.erb until you need to add it
+-store CSS in a public folder
+-store images and javascript in the same public folder
+
+##features
+-eventually this will be a bunch of quotes/images
+-need a slider or a thing to step through
+-test competency in HTML, CSS and basic JS and Sinatra
+
+##how do i
+-get a header that passes links(calls an HTTP method)
+-store the information I want to use in links/images
